51065bf1c396a8c40702c22cc6a34ed8f59cf0b6
Руслан <mal.it.88@mail.ru>
2019-08-29 17:35:06 +0700

    Сверстал блок всё для строительства и адаптировал и слайдер и адаптировал. Сверстал блок инертные материалы частично

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,28 @@ $(document).ready(function(){
     $('#open').on("click", function(){
         $('.mobile-menu').fadeToggle(300)
         $('.menu__btn-burger').toggleClass('active-btn')
-    })
+    });
+
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+              breakpoint: 960,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 764,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+        ]    
+    });
 });
